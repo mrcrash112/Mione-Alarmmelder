c7cb240698031a4b812be2c58e4d69ab30afb0b5
Thorsten Jozefowicz <info@jozefowicz.de>
2026-06-21 12:06:35 +0200

    Release 1.1.9.27 stable

--- Alarmmelder/Properties/AssemblyInfo.cs
+++ Alarmmelder/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a139ff16-f06a-4be5-af0d-52719527477b")]
 [assembly: AssemblyVersion("1.1.9.27")]
 [assembly: AssemblyFileVersion("1.1.9.27")]
-[assembly: AssemblyInformationalVersion("1.1.9.27_Beta")]
+[assembly: AssemblyInformationalVersion("1.1.9.27")]
